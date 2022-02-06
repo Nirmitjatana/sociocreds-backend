8781eb399ae958765739fe56a918b754cd45d9ae
nirmit <nirmitjatana38@gmail.com>
2022-02-06 17:08:34 +0530

    feat: added ngo routes

---
+++ routes/ngo.js
@@ -0,0 +1,26 @@
+const router = require('express').Router()
+const NgoController = require('../controllers/ngo')
+const middlewares = require('../middlewares/auth')
+
+router.post('/login', async (req, res) => {
+  const response = await NgoController.login(req.body.idToken)
+  res.status(response.code).send(response)
+})
+
+router.get('/details', middlewares.isLoggedIn, async (req, res) => {
+  const response = await NgoController.getDetails(req.decoded.id)
+  res.status(response.code).send(response)
+})
+
+router.patch('/update', middlewares.isLoggedIn, async (req, res) => {
+  const response = await NgoController.updateDetails(req.decoded.id, req.body.ngoName, req.body.ngoDescription)
+  res.status(response.code).send(response)
+})
+
+router.post('/post/create', middlewares.isLoggedIn, async (req, res) => {
+  const { postTitle, postDescription, postPhotoUrl } = req.body
+  const response = await NgoController.createPost(req.decoded.id, postTitle, postDescription, postPhotoUrl)
+  res.status(response.code).send(response)
+})
+
+module.exports = router
